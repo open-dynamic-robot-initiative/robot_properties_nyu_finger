48454bf320a5c47167d9c80ecedfaf7cf47de53f
Huaijiang Zhu <hzhu@nyu.edu>
2021-06-22 16:45:19 -0400

    fixed copyrights date

--- src/robot_properties_nyu_finger/config.py
+++ src/robot_properties_nyu_finger/config.py
@@ -3,7 +3,7 @@
 Store the configuration of the Solo family robots.
 
 License: BSD 3-Clause License
-Copyright (C) 2018-2019, New York University , Max Planck Gesellschaft
+Copyright (C) 2018-2021, New York University , Max Planck Gesellschaft
 Copyright note valid unless otherwise stated in individual files.
 All rights reserved.
 """
